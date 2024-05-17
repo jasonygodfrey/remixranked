de30831b9f6164c5a6618295be697f63d6f4b834
Jason <jasonygodfrey@gmail.com>
2024-05-17 12:37:53 -0700

    backend supabase test

---
+++ pages/api/test.js
@@ -0,0 +1,11 @@
+import { supabase } from '../../utils/supabaseClient';
+
+export default async function handler(req, res) {
+  const { data, error } = await supabase.from('users').select('*');
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.status(200).json(data);
+}
